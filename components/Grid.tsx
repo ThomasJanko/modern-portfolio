67824833b39fdd541182bfd8a4d28409d8c589ec
Thomasjan <thomas.jankowski@gestimum.com>
2024-06-03 23:50:22 +0200

    Grid

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
+import { gridItems } from '@/data'
 
 const Grid = () => {
   return (
-    <div>Grid</div>
+    <section id="about">
+      <BentoGrid className="w-full py-20">
+        {gridItems.map
+        (({ id, title, description, className, img, imgClassName, titleClassName, spareImg }) => (
+          <BentoGridItem
+            id={id}
+            key={id}
+            title={title}
+            description={description}
+            className={className}
+            img={img}
+            imgClassName={imgClassName}
+            titleClassName={titleClassName}
+            spareImg={spareImg}
+          />
+        ))}
+      </BentoGrid>
+    </section>
   )
 }
 
--- components/ui/ShimmerButton.tsx
+++ components/ui/ShimmerButton.tsx
@@ -17,8 +17,10 @@ const ShimmerButton = ({
  }: ShimmerButtonProps ) => {
 
   return (
-    <button className={`w-full md:w-60 gap-2 md:mt-10 inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 ${otherClasses}`}>
-        
+    <button 
+      className={`w-full relative md:w-60 gap-2 py-2 min-h-12 md:mt-10 inline-flex animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 ${otherClasses}`}
+      onClick={handleClick}
+    >
         {position === 'left' && icon}
         {title}
         {position === 'right' && icon}
